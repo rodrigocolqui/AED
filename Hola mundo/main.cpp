7c128e2f8260f6c42e90ddabc52e1a2c533c1492
rodrigocolqui <49528654+rodrigocolqui@users.noreply.github.com>
2019-04-11 14:42:00 -0300

    Add files via upload

---
+++ Hola mundo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	cout<<"Hola mundo algoritmos";
+	return 0;
+}
