2f7dd56bd5376d941f198bc1e3724b50c2d197b7
rodrigocolqui <49528654+rodrigocolqui@users.noreply.github.com>
2019-04-15 16:03:11 -0300

    Add files via upload

---
+++ SUMA/main.cpp
@@ -0,0 +1,23 @@
+
+/*/
+SUMA DE DOS NUMEROS
+LEGAJO 169127-0 RODRIGO COLQUI
+15/4/2019
+/*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+float num,num2,suma;	
+cout<<"//Suma de dos numeros//"<<endl<<endl;
+cout<<"//Ingrese el primer numero a sumar//"<<endl;
+cin>>num;
+cout<<"//Ingrese el segundo numero a sumar//"<<endl;
+cin>>num2;	
+cout<<endl;   
+suma=num+num2;
+cout<<"La suma de los dos numeros:   "<<suma<<endl;	
+	
+	return 0;
+}
